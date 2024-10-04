a255836ea66fef2763d0c4ecb1249c9c5cc3dcbf
srksifat-dev <srksifat.dev@gmail.com>
2024-10-04 12:02:29 +0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Md. Shoibur Rahman Khan Sifat on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
